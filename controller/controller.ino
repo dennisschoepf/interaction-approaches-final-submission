3e7c000e577163ba3d1bbf032c598094c0ec176d
Dennis Schoepf <dennis@schoepf.co>
2020-07-11 21:07:41 +0200

    Refactor to be able to use sr04 and servo together

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,6 +1,7 @@
+#include <SR04.h>
 #include <Servo.h>
-#include <HCSR04.h>
 
+static boolean executedHostile = false;
 Servo servo;
 
 int rPinLed = 8;
@@ -9,12 +10,14 @@ int bPinLed = 10;
 
 int ultrasonicTriggerPin = 11;
 int ultrasonicEchoPin = 12;
-HCSR04 hc(ultrasonicTriggerPin, ultrasonicEchoPin);
+SR04 sr04 = SR04(ultrasonicEchoPin, ultrasonicTriggerPin);
+long distance;
 
-int buzzerPin = 6;
+int buzzerPin = 2;
 
 void setup() {
-  servo.attach(7);
+  servo.write(90);
+  servo.attach(13);
   
   pinMode(rPinLed, OUTPUT);
   pinMode(gPinLed, OUTPUT);
@@ -24,22 +27,42 @@ void setup() {
 
   Serial.begin(9600);
 }
- 
 
 void loop() {
-  Serial.println( hc.dist() );
-  setRGBColor(0, 0, 0);
-  // servo.write(10);
-  // tone(buzzerPin, 500);
-  delay(500);
-  // noTone(buzzerPin);
-  // servo.write(30);
-  setRGBColor(0, 0, 0);
-  delay(500);
+  distance = sr04.Distance();
+
+  if (distance < 7) {
+    if (executedHostile == false) {
+      hostileBehavior();
+      executedHostile = true;
+    }
+  }
 }
 
 void setRGBColor(int red, int green, int blue) {
   analogWrite(rPinLed, red);
   analogWrite(gPinLed, green);
   analogWrite(bPinLed, blue);
+}
+
+void hostileBehavior() {
+  servo.write(0);
+
+  for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 255; i++) {
+      setRGBColor(i,0,0);
+      tone(buzzerPin, i*2 + 440, 50);
+      delay(5);
+    }
+
+    servo.write(20);
+  
+    for (int i = 255; i > 0; i--) {
+      setRGBColor(i,0,0);
+      tone(buzzerPin, i*2 + 440, 50);
+      delay(5);
+    }
+
+    servo.write(40);
+  }
 }

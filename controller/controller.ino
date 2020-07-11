b99d6f3f1499a7707f83ae8568729e123756ef28
Dennis Schoepf <dennis@schoepf.co>
2020-07-11 15:16:06 +0200

    Setup all different sensors, servo, light ...

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,17 +1,43 @@
 #include <Servo.h>
+#include <HCSR04.h>
 
-// Initialize variables
 Servo servo;
 
+int rPinLed = 8;
+int gPinLed = 9;
+int bPinLed = 10;
+
+int ultrasonicTriggerPin = 11;
+int ultrasonicEchoPin = 12;
+HCSR04 hc(ultrasonicTriggerPin, ultrasonicEchoPin);
+
+int buzzerPin = 6;
+
 void setup() {
-  // Initialize servo
-  servo.attach(0);
-  servo.write(10);
+  servo.attach(7);
+  
+  pinMode(rPinLed, OUTPUT);
+  pinMode(gPinLed, OUTPUT);
+  pinMode(bPinLed, OUTPUT);
+
+  pinMode(buzzerPin, OUTPUT);
+
+  Serial.begin(9600);
 }
  
-// the loop function runs over and over again forever
+
 void loop() {
-  servo.write(170);
-  delay(2000);
-  servo.write(10);
+  Serial.println( hc.dist() );
+  setRGBColor(0, 0, 0);
+  // tone(buzzerPin, 500);
+  delay(500);
+  // noTone(buzzerPin);
+  setRGBColor(0, 0, 0);
+  delay(500);
+}
+
+void setRGBColor(int red, int green, int blue) {
+  analogWrite(rPinLed, red);
+  analogWrite(gPinLed, green);
+  analogWrite(bPinLed, blue);
 }
